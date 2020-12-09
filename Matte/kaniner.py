8e8dd73fc08050e18a62fabfbf893bb0909ea593
andreas1101 <a-lsundb@hotmail.com>
2020-12-09 15:58:50 +0100

    diff2

--- Matte/kaniner.py
+++ Matte/kaniner.py
@@ -1,9 +1,6 @@
-
-
 def diff():
     count = 0
     n = 1
-    #r = 1.8
     x = float(input("Your value for x0: "))
     r = float(input("Your value for r: "))
 
@@ -14,5 +11,19 @@ def diff():
         n = n + 1
         count = count + 1
 
+def diff2():
+    x = 0.7     # x_0 starting value
+
+    for r in range(2, 10, 1):
+        count = 0
+        n = 1
+        print("r = {}".format(r))
+        while count < 10:
+            x = r*x*(1-x)
+            print("x{} = {}".format(n, x))
+            
+            n = n + 1
+            count = count + 1
+
 if __name__ == "__main__":
-    diff()
+    diff2()
