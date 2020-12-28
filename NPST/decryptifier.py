b3ba58b089ee45fcf0409bd1e9b8adbd9463705b
Andreas1101 <a-lsundb@hotmail.com>
2020-12-28 22:42:49 +0100

    comments

--- NPST/decryptifier.py
+++ NPST/decryptifier.py
@@ -1,5 +1,5 @@
 def decrypt():
-    a_string = "JgkJqPåGtFgvLwnKilgp"
+    a_string = "JgkJqPåGtFgvLwnKilgp"   # Insert your string here.
     alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","æ","ø","å"]
     big_alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Æ","Ø","Å"]
     answer = []
