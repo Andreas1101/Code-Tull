ff4b229c57c61fea5a18b0158fdec80e9af5a759
andreas1101 <a-lsundb@hotmail.com>
2023-02-06 20:07:59 +0100

    update rust script

--- rust_server/start.bat
+++ rust_server/start.bat
@@ -1,4 +1,4 @@
 echo off
 :start 
-C:\server\steamcmd\steamcmd.exe +login anonymous +force_install_dir c:\server\rustserver\ +app_update 258550 +quit
-RustDedicated.exe -batchmode +server.port 28015 +server.level "Procedural Map" +server.seed 1234 +server.worldsize 4000 +server.maxplayers 10  +server.hostname "Jørgen server" +server.description "Bare for harde kjerner." +server.identity "server1" +rcon.port 28016 +rcon.password letmein +rcon.web 1
+C:\steamcmd\steamcmd.exe +force_install_dir +login anonymous c:\rustserver\ +app_update 258550 +quit
+RustDedicated.exe -batchmode +server.port 28015 +server.level "Procedural Map" +server.seed 92946045 +server.worldsize 3600 +server.maxplayers 10  +server.hostname "Gulag" +server.description "Bare for harde kjerner." +server.identity "server1" +rcon.port 28016 +rcon.password letmein +rcon.web 1
